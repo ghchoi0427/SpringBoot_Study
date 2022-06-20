381c09824ea725b37eb32df76531573e6dab03c7
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-20 20:08:28 +0900

    feat: 정률 할인정책 생성

---
+++ src/main/java/com/dust/hello/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package com.dust.hello.discount;
+
+import com.dust.hello.member.Grade;
+import com.dust.hello.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    int discountRate = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountRate / 100;
+        }
+        return 0;
+    }
+}
