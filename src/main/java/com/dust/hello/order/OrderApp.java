fd7dabe3d0f772027fcb1f17a36289bfac0515a9
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-20 20:08:51 +0900

    fix: 의존관계 주입

--- src/main/java/com/dust/hello/order/OrderApp.java
+++ src/main/java/com/dust/hello/order/OrderApp.java
@@ -1,15 +1,16 @@
 package com.dust.hello.order;
 
+import com.dust.hello.AppConfig;
 import com.dust.hello.member.Grade;
 import com.dust.hello.member.Member;
 import com.dust.hello.member.MemberService;
-import com.dust.hello.member.MemberServiceImpl;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
