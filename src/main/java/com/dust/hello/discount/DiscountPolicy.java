77e3685264d4ad45c896defefc972d947f841439
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-20 15:46:03 +0900

    feat: 주문 정책 인터페이스 생성

---
+++ src/main/java/com/dust/hello/discount/DiscountPolicy.java
@@ -0,0 +1,12 @@
+package com.dust.hello.discount;
+
+import com.dust.hello.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     *
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+}
