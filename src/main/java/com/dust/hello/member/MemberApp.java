8776fd1321092deadaa4dcd46f1571712bcab741
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-20 15:46:44 +0900

    feat: 메인함수 생성

---
+++ src/main/java/com/dust/hello/member/MemberApp.java
@@ -0,0 +1,16 @@
+package com.dust.hello.member;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.joinMember(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member: " + member.getName());
+        System.out.println("find member: " + findMember.getName());
+        System.out.println(member.equals(findMember));
+
+    }
+}
---
+++ src/main/java/com/dust/hello/order/OrderApp.java
@@ -0,0 +1,23 @@
+package com.dust.hello.order;
+
+import com.dust.hello.member.Grade;
+import com.dust.hello.member.Member;
+import com.dust.hello.member.MemberService;
+import com.dust.hello.member.MemberServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.joinMember(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order.toString());
+        System.out.println("order.calculatePrice = " + order.calculatePrice());
+    }
+}
