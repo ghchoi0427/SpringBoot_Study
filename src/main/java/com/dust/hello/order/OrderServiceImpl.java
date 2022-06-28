ff7a13f5c223eb22e026681b27494f7b88f5821a
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-28 18:14:24 +0900

    fix: @Component 어노테이션 설정

--- src/main/java/com/dust/hello/order/OrderServiceImpl.java
+++ src/main/java/com/dust/hello/order/OrderServiceImpl.java
@@ -3,12 +3,16 @@ package com.dust.hello.order;
 import com.dust.hello.discount.DiscountPolicy;
 import com.dust.hello.member.Member;
 import com.dust.hello.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
